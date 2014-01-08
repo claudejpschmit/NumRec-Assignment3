3f136e79ce7d64c24811faf9ab8a4eecceab2e27
Claude Schmit <claudejpschmit@gmailcom>
2014-01-08 16:34:06 +0000

    Tidying up main

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,7 @@
 
 typedef unsigned int uint ;
 
-#define INPUTFORM (1.0 + cos((period/N)*ii)  + cos((3*period/N)*ii)  )
+//Convenient variable definitions
 #define XMAX 4.0
 #define ITERATIONS 1000
 #define DX XMAX/(double)ITERATIONS
@@ -26,12 +26,16 @@ using namespace std;
 
 int main( ) {
 
+    //instantiate electric field ODE and Method to solve it
     PNJUNCTION::ElectricFieldODE E_ODE(X0, H);
-
     PNJUNCTION::FORKMethod F_method_efield(DX, &E_ODE, X0);
+
+    //creating output file
     ofstream solution("solution_electric.txt");
     solution << X0 << " " << E_ODE.initial_value() << endl;
 
+    //creating vector to contain values of the electric field
+    //and writing data to file
     std::vector<double> efield;
     efield.push_back(E_ODE.initial_value());
     for (int i = 1; i < ITERATIONS; ++i) {
@@ -41,90 +45,78 @@ int main( ) {
     }
     solution.close();
 
+    //instantiate potential field ODE and Method to solve it using the result
+    //from the electric field analysis.
     PNJUNCTION::PotentialODE P_ODE(X0, H, efield, DX);
     PNJUNCTION::FORKMethod F_method_potential(DX, &P_ODE, X0);
 
+    //creating output file and writing data to it.
     ofstream solutionP("solution_potential.txt");
     solutionP << X0 << " " << P_ODE.initial_value() << endl;
-
     for (int i = 1; i < ITERATIONS; ++i) {
         solutionP << i * DX << " " << F_method_potential.step() << endl;
     }
     solutionP.close();
 
-    ofstream solutionE("solution_potential_exact.txt");
-
+    //creating exact solutions for comparison
+    ofstream solutionPE("solution_potential_exact.txt");
     for (int i = 0; i < ITERATIONS; ++i) {
-        solutionE << i * DX << " " << P_ODE.exact_solution(i * DX) << endl;
+        solutionPE << i * DX << " " << P_ODE.exact_solution(i * DX) << endl;
     }
-    solutionE.close();
+    solutionPE.close();
     ofstream solutionEE("solution_electric_exact.txt");
-
     for (int i = 0; i < ITERATIONS; ++i) {
         solutionEE << i * DX << " " << E_ODE.exact_solution(i * DX) << endl;
     }
     solutionEE.close();
 
 
-    //////////////////////////////////////////////////// FFTW
-    //N = Number of Bins -> more bins = better result
-    //
-    //TODO: COMPLEX VALUES STILL WRONG
+    //  ********* Fourier Transform analysis ********** //
+    // N bins
     int N=500 ;
     fftw_complex *rho, *rho_ft, *e_ft, *e;
     fftw_plan p;
-    //
-    double twopi = 2.0*PI ;
-    double period = twopi;
-    // this was: double period = twopi * 4.0;
-    //....	
 
-    ofstream fft("fft_result.txt");
+    //creating arrays and output files to hold solutions.
+    ofstream fft("solution_electric_fft.txt");
     rho = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
     rho_ft = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
     e = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
     e_ft = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
 
+    //plan for forward fourier transforming the charge density
     p = fftw_plan_dft_1d(N, rho, rho_ft, FFTW_FORWARD, FFTW_ESTIMATE);
 
-    cout << "Input - REAL - IMAG" << endl;
     for(int n=0; n < N ; ++n) { 
         rho[n][0] = E_ODE.first_derivative(0.0, n * XMAX/(double)N);
         rho[n][1] = 0.0;
-        cout << n * (XMAX/(double)N) << " " << rho[n][0] << " " << rho[n][1] << endl;
     }
-    //...
 
+    //Fourier transforming the charge density
     fftw_execute(p); /* repeat as needed */
 
-    //...
 
-    cout << " Results of fftw run " << endl ;
+    // divide rho_ft by ik_n
     for(int n=0; n < N ; ++n) { 
-        cout << fixed ;
-        cout << setprecision(1) ;
-        cout << rho_ft[n][0]/(N/2.0) << " " << rho_ft[n][1]/(N/2.0) << endl ;
-        // divide rho_ft by ik_n
         e_ft[n][0] = rho_ft[n][1]/(PI * n);
         e_ft[n][1] = - rho_ft[n][0] / (PI * n);
-        //os << ii+1 << " "  << out[ii][0]/1000. << endl ;
     }
     e_ft[0][0] = 0;
     e_ft[0][1] = 0;
 
+    // Inverse Fourier transform of the Electric Field
     p = fftw_plan_dft_1d(N, e_ft, e, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
 
-    fftw_execute(p); /* repeat as needed */
-
-    cout << " Results of inverse " << endl ;
+    // writing the normalised values to file
     for(int n=0; n < N ; ++n) { 
-        cout << fixed ;
-        cout << setprecision(1) ;
-        cout << e[n][0]/N <<  "   "  << e[n][1]/N << endl;
-        fft << n * (XMAX/(double)N) << " " << (H/(((e[N/2][0]/(double)N) - (e[0][0]/(double)N)))) * ((e[n][0]/(double)N) - (e[0][0]/(double)N)) << endl;
+        fft << n * (XMAX/(double)N) << " " << 
+            (H/(((e[N/2][0]/(double)N) - (e[0][0]/(double)N)))) * 
+            ((e[n][0]/(double)N) - (e[0][0]/(double)N)) << endl;
     }
     fft.close(); 
 
+    //Clean up
     fftw_destroy_plan(p);
     fftw_free(rho); fftw_free(rho_ft);
 
